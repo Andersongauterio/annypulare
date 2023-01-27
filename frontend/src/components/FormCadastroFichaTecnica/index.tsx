5bb907e17f73de9f17e32ffcbd88559e315ab374
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-01-26 22:02:11 -0300

    feat: cadastro ficha tecnica component creation

---
+++ frontend/src/components/FormCadastroFichaTecnica/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+const FormCadastroFichaTecnica = () => {
+    return (
+        <div className="annypulare-form-cad-ficha-tecnica-container">
+
+        </div>
+    );
+};
+
+export default FormCadastroFichaTecnica;
