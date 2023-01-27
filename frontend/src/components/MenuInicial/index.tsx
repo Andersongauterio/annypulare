e17bd880fb257f0af1dd53d2fe59cb629ec9b87d
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-01-26 22:00:04 -0300

    feat: menu initial component creation

---
+++ frontend/src/components/MenuInicial/index.tsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom';
+import './styles.css';
+
+const MenuInicial = () => {
+    return (
+        <div className="annypulare-menu-inicial-container">
+            <Link to="/manterFichaTecnica">
+                <button className="btn btn-primary annypulare-btn">
+                    <h6>Manter Ficha Técnica</h6>
+                </button>
+            </Link>
+            <Link to="/manterCategoriaProduto">
+                <button className="btn btn-primary annypulare-btn">
+                    <h6>Manter Categoria de Produto</h6>
+                </button>
+            </Link>
+            <Link to="/manterInsumo">
+                <button className="btn btn-primary annypulare-btn">
+                    <h6>Manter Insumo</h6>
+                </button>
+            </Link>
+
+        </div>
+    );
+};
+
+export default MenuInicial;
