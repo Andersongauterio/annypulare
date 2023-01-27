fa874de5d6c250814f7a5170029ba6b82d2b241d
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-01-26 21:58:32 -0300

    feat: manter insumo page initial

---
+++ frontend/src/pages/ManterInsumo/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+const ManterInsumo = () => {
+    return (
+        <div className="annypulare-manter-insumo-container">
+
+        </div>
+    );
+};
+
+export default ManterInsumo;
