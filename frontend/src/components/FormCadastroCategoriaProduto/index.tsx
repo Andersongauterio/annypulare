dbe0313022ff82785b8dccf3c4f734dfff783027
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-01-26 21:58:04 -0300

    feat: FormCategoriaProduto component

---
+++ frontend/src/components/FormCadastroCategoriaProduto/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+const FormCadastroCategoriaProduto = () => {
+    return (
+        <div className="annypulare-form-cad-cat-produto-container">
+            <input type="text" />
+            <h1>test</h1>
+        </div>
+    );
+};
+
+export default FormCadastroCategoriaProduto;
