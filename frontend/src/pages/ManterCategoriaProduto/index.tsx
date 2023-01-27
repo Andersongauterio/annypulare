3a2e2a1367b689b454800a38ee5b0b757498c8d5
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-01-26 21:59:23 -0300

    feat: manter categoria component creation

---
+++ frontend/src/pages/ManterCategoriaProduto/index.tsx
@@ -0,0 +1,12 @@
+import FormCadastroCategoriaProduto from '../../components/FormCadastroCategoriaProduto';
+import './styles.css';
+
+const ManterCategoriaProduto = () => {
+    return (
+        <div className="annypulare-manter-categoria-produto-container">
+            <FormCadastroCategoriaProduto />
+        </div>
+    );
+};
+
+export default ManterCategoriaProduto;
