b1ab83f28267ea247d49c62fbd8f6d1474e84920
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-01-28 22:11:37 -0300

    feat: form cad insumo v1

--- frontend/src/components/FormCadastroInsumo/index.tsx
+++ frontend/src/components/FormCadastroInsumo/index.tsx
@@ -1,9 +1,30 @@
+import DefaultButtons from '../DefaultButtons';
 import './styles.css';
 
 const FormCadastroInsumo = () => {
     return (
-        <div className="annypulare-form-cad-insumo-container">
-
+        <div className="annypulare-form-cad-insumo-container annypulare-form-container">
+            <h2>Informações do Insumo:</h2>
+            <form className="annypulare-form-cad-insumo-form">
+                <div className="annypulare-form-cad-insumo-info annypulare-form-box">
+                        <input 
+                            type="text" 
+                            name="nome" 
+                            className="form-control base-input" 
+                            placeholder="Nome:"/>
+                        <input 
+                            type="text" 
+                            name="descricao" 
+                            className="form-control base-input" 
+                            placeholder="Descrição:"/>
+                        <input 
+                            type="text" 
+                            name="unidadeMedida" 
+                            className="form-control base-input" 
+                            placeholder="Unidade de medida:"/>
+                </div>
+                <DefaultButtons />
+            </form>
         </div>
     );
 };
