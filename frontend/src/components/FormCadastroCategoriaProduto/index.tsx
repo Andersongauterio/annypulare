047eafd817484ae0694deea97c1ef245e675e4ed
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-01-28 22:10:58 -0300

    feat: form cad cat produto v1

--- frontend/src/components/FormCadastroCategoriaProduto/index.tsx
+++ frontend/src/components/FormCadastroCategoriaProduto/index.tsx
@@ -1,10 +1,25 @@
+import DefaultButtons from '../DefaultButtons';
 import './styles.css';
 
 const FormCadastroCategoriaProduto = () => {
     return (
-        <div className="annypulare-form-cad-cat-produto-container">
-            <input type="text" />
-            <h1>test</h1>
+        <div className="annypulare-form-cad-cat-produto-container annypulare-form-container">
+            <form className="annypulare-form-cad-cat-produto-form">
+                <h2>Categoria do Produto</h2>
+                <div className="annypulare-form-cad-cat-produto-info annypulare-form-box">
+                    <input 
+                        type="text" 
+                        name="nome" 
+                        className="form-control base-input" 
+                        placeholder="Nome:"/>
+                    <input 
+                        type="text" 
+                        name="descricao" 
+                        className="form-control base-input" 
+                        placeholder="Descrição:"/>
+                </div>
+                <DefaultButtons />
+            </form>
         </div>
     );
 };
