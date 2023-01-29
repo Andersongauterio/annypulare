813f6c0c9a5add43104f9089b8eca4a71d90a97c
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-01-28 22:10:34 -0300

    feat: default button component

---
+++ frontend/src/components/DefaultButtons/index.tsx
@@ -0,0 +1,17 @@
+import './styles.css';
+
+const DefaultButtons = () => {
+    return (
+        <div className="annypulare-default-buttons-container">
+            <button
+                className="btn btn-outline-danger">
+                CANCELAR
+            </button>
+            <button className="btn btn-primary text-white">
+                SALVAR
+            </button>
+        </div>
+    );
+};
+
+export default DefaultButtons; 
